1fedc127e0e41deb722cb685cc66d60f889fae89
Varindra <varindrahart@gmail.com>
2015-06-07 15:58:28 -0400

    commit1

---
+++ madLibs/madLibs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  madLibs
+//
+//  Created by Varindra Hart on 6/7/15.
+//  Copyright (c) 2015 Varindra Hart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ madLibs/madLibs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  madLibs
+//
+//  Created by Varindra Hart on 6/7/15.
+//  Copyright (c) 2015 Varindra Hart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
